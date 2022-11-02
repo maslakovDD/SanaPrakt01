682d70290f3c8fe29f9bc5ff35a3f39d39d48198
maslakovDD <ipz212_mdd@student.ztu.edu.ua>
2022-11-02 17:36:49 +0200

    added new project: print name and surname

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Маслаков Дмитро");
+        }
+    }
+}
